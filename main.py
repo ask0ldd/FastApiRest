7f48bec6ae9c7b7891d31d886aba47dff93441d9
Cedric B-Eff <cedrikopenclass@gmx.com>
2023-08-15 01:00:16 +0200

    transactions endpoint

--- main.py
+++ main.py
@@ -30,3 +30,14 @@ def read_cryptos(client_id : int):
         {"currency" : "ethereum", "value" : 3277, "amount" : 0.3, "lastmonthvalue" : 3000},
         {"currency" : "binance coin", "value" : 240, "amount" : 10, "lastmonthvalue" : 260},
     ]
+
+@app.get("/transactions/last6/{client_id}")
+def read_transactions(client_id : int):
+    return [
+        {"recipient" : "Uber Taxi", "date" : "02-13-2012", "amount" : 948.55},
+        {"recipient" : "AT&T", "date" : "02-14-2012", "amount" : 19.99},
+        {"recipient" : "Ebay", "date" : "02-18-2012", "amount" : 98.55},
+        {"recipient" : "Ebay", "date" : "02-19-2012", "amount" : 45.50},
+        {"recipient" : "Air BnB", "date" : "02-22-2012", "amount" : -120.00},
+        {"recipient" : "Direct Energy", "date" : "02-23-2012", "amount" : -60.00}
+    ]
